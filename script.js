3e0e9ba42dde14f92bf9436081431ddd3feb0f5b
Abhishek Sharma <abhishekumar4165@gmail.com>
2025-03-05 20:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+async function msg() {
+	let promise = new Promise ((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(1);
+		},2000)
+	})
+	let res = await promise;
+	console.log("")
+}
